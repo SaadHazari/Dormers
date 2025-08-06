b7570ef99a7dcf1a7f7484c7a745e2564009e050
Adil nawaz <adilnawaz256@gmail.com>
2025-08-06 21:34:12 +0530

    add testmonial

--- src/app/components/TestimonialsBubbles.tsx
+++ src/app/components/TestimonialsBubbles.tsx
@@ -4,72 +4,156 @@ import { useTheme } from "next-themes";
 import Image from "next/image";
 import { useSwipeable } from "react-swipeable";
 
-const messages = [
-  {
-    id: 1,
-    from: "bot",
-    name: "Majid Alamoudi",
-    rating: 5,
-    text: "The quality of the food was superb and I really liked their spicy sauce as well. Definitely recommend it.",
-  },
-  {
-    id: 2,
-    from: "user",
-    name: "Hades Geolbar",
-    rating: 5,
-    text: "I tried dormers food for a week now and the food has been very good and the quality consistent over every of my dishes. The price is also affordable. I look forward to order more from DORMERS.",
-  },
-  {
-    id: 3,
-    from: "bot",
-    name: "Mohmmed Saif",
-    rating: 5,
-    text: "One of the tastiest meal plan I have taste so far with having a range of variety of cuisine and a very well balanced meal over all value for money very inter rated.",
-  },
-  {
-    id: 4,
-    from: "user",
-    name: "Niaz Mukati",
-    rating: 5,
-    text: "Food was absolutely phenomenal and it lasted me 1½ meals for the price of half a meal from most restaurants.",
-  },
-  {
-    id: 5,
-    from: "bot",
-    name: "Rehan Khoja",
-    rating: 5,
-    text: "Just had an incredible biryani that every dormer needs to try! The rice was perfectly fluffy, full of aromatic spices, and paired beautifully with the creamy raita. It was so good I ate it way too fast—definitely need a second round. Highly recommend!",
-  },
-  {
-    id: 6,
-    from: "user",
-    name: "Bereket Adane",
-    rating: 5,
-    text: "I've had a great experience with Dormer! The team is super friendly and always ensures everything goes smoothly. The food is fantastic — fresh, tasty, and generous portions. There's a good variety that suits different tastes and diets.",
-  },
-  {
-    id: 7,
-    from: "bot",
-    name: "Hamzah Khodabocus",
-    rating: 6,
-    text: "Very good food and decent price Good for students who don't have time to cook and want a healthy alternative",
-  },
-  {
-    id: 8,
-    from: "user",
-    name: "Ibrahim Khan Amjad",
-    rating: 5,
-    text: "The food was nice. It was flavorful, balanced portion, balanced taste. Good job.",
-  },
-  {
-    id: 9,
-    from: "bot",
-    name: "Mundhir Al Said ",
-    rating: 5,
-    text: "It was really tasty. Thank you!",
-  },
-];
-
+const messages =
+  [
+    {
+      "id": 1,
+      "from": "bot",
+      "name": "Majid Almoudi",
+      "city": "KSK Homes",
+      "text": "The quality of the food was superb and really liked their spicy sauce as well. Definitely recommend it."
+    },
+    {
+      "id": 2,
+      "from": "user",
+      "name": "Hadee Goolbar",
+      "city": "Yugo",
+      "text": "I tried dormers food for a week now and the food has been very good and the quality consistent over every of my dishes. The price is also affordable. I look forward to order more from Dormers."
+    },
+    {
+      "id": 3,
+      "from": "bot",
+      "name": "Niaz Mukati",
+      "city": "The Myriad",
+      "text": "Food was absolutely phenomenal, and it lasted me 1 ½ meals for the price of half a meal from most restaurants."
+    },
+    {
+      "id": 4,
+      "from": "user",
+      "name": "Rehan Khoja",
+      "city": "KSK Homes",
+      "text": "Just has an incredible biryani that every dormer needs to try! The rice was perfectly fluffy, full of aromatic spices, and paired with beautifully with the creamy raita. It was so good I at it way too fast definitely need a second round. Highly recommend!"
+    },
+    {
+      "id": 5,
+      "from": "bot",
+      "name": "Hamzah Khodabocus",
+      "city": "Yugo",
+      "text": "Very good food and decent price. Good for students who don't have time to cook and want a healthy alternative."
+    },
+    {
+      "id": 6,
+      "from": "user",
+      "name": "Ibrahim Khan Amjad",
+      "city": "Yugo",
+      "text": "The food was nice. It was flavorful. Balanced portion, balanced taste. Good job."
+    },
+    {
+      "id": 7,
+      "from": "bot",
+      "name": "Mundhir Ali Said",
+      "city": "The Myriad",
+      "text": "It was really tasty Thank you!"
+    },
+    {
+      "id": 8,
+      "from": "user",
+      "name": "Bereket Adane",
+      "city": "KSK Homes",
+      "text": "Bro good afternoon. The food today was WOW ! you have no clue how I love it I have never tasted a coconut before in a food but it was magical. This is my favorite of them all."
+    },
+    {
+      "id": 9,
+      "from": "bot",
+      "name": "Mohmmed Saif",
+      "city": "The Myriad",
+      "text": "One of the tastiest meal plan I have taste so far with having a range of variety of cuisine and a very well balanced meal. Overall, value for money."
+    },
+    {
+      "id": 10,
+      "from": "user",
+      "name": "Sarah Sherali",
+      "city": "The Myriad",
+      "text": "The service provided by dormer's is amazing, they deliver food on time and the food tastes really good which meets ur preferences. Thank you so much, hope the feedback can be helpful someday."
+    },
+    {
+      "id": 11,
+      "from": "bot",
+      "name": "Jannat Kona",
+      "city": "Yugo",
+      "text": "Had a great dinner meal from Dormer's. It was a chicken and egg fried rice. The combination was good. The packaging was good. A bit of more salt and some veggies would have been perfect! A platter including this will be lovely."
+    },
+    {
+      "id": 12,
+      "from": "user",
+      "name": "Bello Hashim",
+      "city": "The Myriad",
+      "text": "Amazing, filling, delicious, good food Their service is wonderful I highly recommend!"
+    },
+    {
+      "id": 13,
+      "from": "bot",
+      "name": "Angela Roy",
+      "city": "Yugo",
+      "text": "some of the best homemade food!! both delicious and healthy"
+    },
+    {
+      "id": 14,
+      "from": "user",
+      "name": "Labeeq Dawre",
+      "city": "Yugo",
+      "text": "Really delicious. The biryani hits the spot"
+    },
+    {
+      "id": 15,
+      "from": "bot",
+      "name": "Syed Abdul Basit",
+      "city": "Yugo",
+      "text": "So far the best quality of food i have tasted. I loved the food. it was amazing & everything simply balanced."
+    },
+    {
+      "id": 16,
+      "from": "user",
+      "name": "Hachem",
+      "city": "KSK homes",
+      "text": "For me the rice was cooked well, and I liked the idea of adding raisins. It added a wonderful flavor."
+    },
+    {
+      "id": 17,
+      "from": "bot",
+      "name": "Suhani Khera",
+      "city": "Vogo Grand",
+      "text": "Love your food, I have Withdrawals."
+    },
+    {
+      "id": 18,
+      "from": "user",
+      "name": "Mirza Alamdar Hussein",
+      "city": "Vogo Grand",
+      "text": "Boss loved the food. Quantity + Quality. Matched the level of expectations."
+    },
+    {
+      "id": 19,
+      "from": "bot",
+      "name": "Rabiya Bi",
+      "city": "KSK Homes",
+      "text": "Alhamdulillah the food has been good. I feel like personally for me, it's a little less spice but it's good. A lot better than the other tiffin services in terms of quality. Thank you."
+    },
+    {
+      "id": 20,
+      "from": "user",
+      "name": "Hafsa Ayesha",
+      "city": "Yugo",
+      "text": "Hey. The food is soooo good. The taste and everything is 10/10/ reminded me of my mom's food back home in Pakistan 🫶. Loved every bit of it."
+    },
+    {
+      "id": 21,
+      "from": "bot",
+      "name": "Adriel Almeida",
+      "city": "Yugo",
+      "text": "...and Ive tried a few meal plans before but nothing compares to this..."
+    }
+  ]
 export default function TestimonialsBubbles() {
   const { theme } = useTheme();
   const [currentGroup, setCurrentGroup] = useState(0);
@@ -89,23 +173,23 @@ export default function TestimonialsBubbles() {
     return () => clearInterval(interval);
   }, [groupedMessages.length]);
   const handleDotClick = (index: number) => setCurrentGroup(index);
-const swipeHandlers = useSwipeable({
-  onSwipedLeft: () =>
-    setCurrentGroup((prev) => (prev + 1) % groupedMessages.length),
-  onSwipedRight: () =>
-    setCurrentGroup((prev) =>
-      prev === 0 ? groupedMessages.length - 1 : prev - 1
-    ),
-  trackTouch: true,
-  trackMouse: true, // <---- change this
-  preventScrollOnSwipe: true,
-  delta: 50,
-});
+  const swipeHandlers = useSwipeable({
+    onSwipedLeft: () =>
+      setCurrentGroup((prev) => (prev + 1) % groupedMessages.length),
+    onSwipedRight: () =>
+      setCurrentGroup((prev) =>
+        prev === 0 ? groupedMessages.length - 1 : prev - 1
+      ),
+    trackTouch: true,
+    trackMouse: true, // <---- change this
+    preventScrollOnSwipe: true,
+    delta: 50,
+  });
 
 
   return (
-    <div   className="relative bg-[#031624] py-6 pb-0 w-screen overflow-hidden -mx-[calc((100vw_-_100%)/2)] touch-pan-y"
-  {...swipeHandlers}>
+    <div className="relative bg-[#031624] py-6 pb-0 w-screen overflow-hidden -mx-[calc((100vw_-_100%)/2)] touch-pan-y"
+      {...swipeHandlers}>
       <div className="flex flex-col items-center gap-4 min-h-[200px] justify-center">
         <AnimatePresence mode="wait">
           {groupedMessages[currentGroup].map((msg) => (
@@ -115,39 +199,36 @@ const swipeHandlers = useSwipeable({
               animate={{ opacity: 1, x: 0 }}
               exit={{ opacity: 0, x: msg.from === "user" ? -100 : 100 }}
               transition={{ duration: 0.6, ease: "easeInOut" }}
-              className={`max-w-[250px] p-4 rounded-2xl ${
-                theme === "light"
-                  ? "bg-[#1E3A4F] text-white"
-                  : "bg-[#EEE9DA] text-[#1E3A4F]"
-              } ${
-                msg.from === "user" ? "self-end mr-8" : "self-start ml-8"
-              } relative`}
+              className={`max-w-[250px] p-4 rounded-2xl ${theme === "light"
+                ? "bg-[#1E3A4F] text-white"
+                : "bg-[#EEE9DA] text-[#1E3A4F]"
+                } ${msg.from === "user" ? "self-end mr-8" : "self-start ml-8"
+                } relative`}
             >
               {/* Name & Stars */}
               <div className="flex items-center justify-between mb-2">
-                <div className="flex items-center gap-2">
+                {/* <div className="flex items-center gap-2">
                   <div className="w-6 h-6 rounded-full bg-[#1E3A4F] flex items-center justify-center text-white text-xs font-bold uppercase">
                     {msg.name?.charAt(0)}
                   </div>
                   <span className="text-sm font-semibold">{msg.name}</span>
+                </div> */}
+                <div className="flex items-center gap-2">
+                  <div className="w-6 h-6 rounded-full bg-[#1E3A4F] flex items-center justify-center text-white text-xs font-bold uppercase">
+                    {msg.name?.charAt(0)}
+                  </div>
+                  <div className="flex flex-col">
+                    <span className="text-sm font-semibold">{msg.name}</span>
+                    <span className="text-xs font-normal">{msg.city}</span>
+                  </div>
                 </div>
-                <div className="flex gap-0.5">
-                  {[...Array(msg.rating)].map((_, i) => (
-                    <Image
-                      key={i}
-                      src="/images/starticon.svg"
-                      alt=""
-                      width={12}
-                      height={12}
-                      className="w-[8px] h-[8px]"
-                      style={{
-                        imageRendering: "crisp-edges",
-                        backgroundRepeat: "repeat",
-                      }}
-                      priority
-                    />
+                {/* <div className="flex gap-0.5">
+                  {[...Array(msg.city)].map((text, i) => (
+
+                    <p>{text}</p>
+                  
                   ))}
-                </div>
+                </div> */}
               </div>
 
               {/* Message Text */}
@@ -164,11 +245,9 @@ const swipeHandlers = useSwipeable({
 
               {/* Message tail */}
               <div
-                className={`absolute -bottom-2 ${
-                  msg.from === "user" ? "right-4" : "left-4"
-                } w-4 h-4 transform rotate-45 ${
-                  theme === "light" ? "bg-[#1E3A4F]" : "bg-[#EEE9DA]"
-                }`}
+                className={`absolute -bottom-2 ${msg.from === "user" ? "right-4" : "left-4"
+                  } w-4 h-4 transform rotate-45 ${theme === "light" ? "bg-[#1E3A4F]" : "bg-[#EEE9DA]"
+                  }`}
               />
             </motion.div>
           ))}
@@ -179,11 +258,10 @@ const swipeHandlers = useSwipeable({
           <button
             key={index}
             onClick={() => handleDotClick(index)}
-            className={`w-2.5 h-2.5 rounded-full transition-all duration-300 ${
-              index === currentGroup
-                ? "bg-white scale-110 w-[60px] h-[8px]"
-                : "bg-white/30"
-            }`}
+            className={`w-2.5 h-2.5 rounded-full transition-all duration-300 ${index === currentGroup
+              ? "bg-white scale-110 w-[60px] h-[8px]"
+              : "bg-white/30"
+              }`}
           />
         ))}
       </div>
--- src/app/components/TestmonialsDesktop.jsx
+++ src/app/components/TestmonialsDesktop.jsx
@@ -4,71 +4,156 @@ import { motion, AnimatePresence } from "framer-motion";
 import { useTheme } from "next-themes";
 import Image from "next/image";
 
-const messages = [
-  {
-    id: 1,
-    from: "bot",
-    name: "Majid Alamoudi",
-    rating: 5,
-    text: "The quality of the food was superb and I really liked their spicy sauce as well. Definitely recommend it.",
-  },
-  {
-    id: 2,
-    from: "user",
-    name: "Hades Geolbar",
-    rating: 5,
-    text: "I tried dormers food for a week now and the food has been very good and the quality consistent over every of my dishes. The price is also affordable. I look forward to order more from DORMERS.",
-  },
-  {
-    id: 3,
-    from: "bot",
-    name: "Mohmmed Saif",
-    rating: 5,
-    text: "One of the tastiest meal plan I have taste so far with having a range of variety of cuisine and a very well balanced meal over all value for money very inter rated.",
-  },
-  {
-    id: 4,
-    from: "user",
-    name: "Niaz Mukati",
-    rating: 5,
-    text: "Food was absolutely phenomenal and it lasted me 1½ meals for the price of half a meal from most restaurants.",
-  },
-  {
-    id: 5,
-    from: "bot",
-    name: "Rehan Khoja",
-    rating: 5,
-    text: "Just had an incredible biryani that every dormer needs to try! The rice was perfectly fluffy, full of aromatic spices, and paired beautifully with the creamy raita. It was so good I ate it way too fast—definitely need a second round. Highly recommend!",
-  },
-  {
-    id: 6,
-    from: "user",
-    name: "Bereket Adane",
-    rating: 5,
-    text: "I've had a great experience with Dormer! The team is super friendly and always ensures everything goes smoothly. The food is fantastic — fresh, tasty, and generous portions. There's a good variety that suits different tastes and diets.",
-  },
-  {
-    id: 7,
-    from: "bot",
-    name: "Hamzah Khodabocus",
-    rating: 6,
-    text: "Very good food and decent price Good for students who don't have time to cook and want a healthy alternative",
-  },
-  {
-    id: 8,
-    from: "user",
-    name: "Ibrahim Khan Amjad",
-    rating: 5,
-    text: "The food was nice. It was flavorful, balanced portion, balanced taste. Good job.",
-  },
-  {
-    id: 9,
-    from: "bot",
-    name: "Mundhir Al Said ",
-    rating: 5,
-    text: "It was really tasty. Thank you!",
-  },
-];
+const messages =
+  [
+    {
+      "id": 1,
+      "from": "bot",
+      "name": "Majid Almoudi",
+      "city": "KSK Homes",
+      "text": "The quality of the food was superb and really liked their spicy sauce as well. Definitely recommend it."
+    },
+    {
+      "id": 2,
+      "from": "user",
+      "name": "Hadee Goolbar",
+      "city": "Yugo",
+      "text": "I tried dormers food for a week now and the food has been very good and the quality consistent over every of my dishes. The price is also affordable. I look forward to order more from Dormers."
+    },
+    {
+      "id": 3,
+      "from": "bot",
+      "name": "Niaz Mukati",
+      "city": "The Myriad",
+      "text": "Food was absolutely phenomenal, and it lasted me 1 ½ meals for the price of half a meal from most restaurants."
+    },
+    {
+      "id": 4,
+      "from": "user",
+      "name": "Rehan Khoja",
+      "city": "KSK Homes",
+      "text": "Just has an incredible biryani that every dormer needs to try! The rice was perfectly fluffy, full of aromatic spices, and paired with beautifully with the creamy raita. It was so good I at it way too fast definitely need a second round. Highly recommend!"
+    },
+    {
+      "id": 5,
+      "from": "bot",
+      "name": "Hamzah Khodabocus",
+      "city": "Yugo",
+      "text": "Very good food and decent price. Good for students who don't have time to cook and want a healthy alternative."
+    },
+    {
+      "id": 6,
+      "from": "user",
+      "name": "Ibrahim Khan Amjad",
+      "city": "Yugo",
+      "text": "The food was nice. It was flavorful. Balanced portion, balanced taste. Good job."
+    },
+    {
+      "id": 7,
+      "from": "bot",
+      "name": "Mundhir Ali Said",
+      "city": "The Myriad",
+      "text": "It was really tasty Thank you!"
+    },
+    {
+      "id": 8,
+      "from": "user",
+      "name": "Bereket Adane",
+      "city": "KSK Homes",
+      "text": "Bro good afternoon. The food today was WOW ! you have no clue how I love it I have never tasted a coconut before in a food but it was magical. This is my favorite of them all."
+    },
+    {
+      "id": 9,
+      "from": "bot",
+      "name": "Mohmmed Saif",
+      "city": "The Myriad",
+      "text": "One of the tastiest meal plan I have taste so far with having a range of variety of cuisine and a very well balanced meal. Overall, value for money."
+    },
+    {
+      "id": 10,
+      "from": "user",
+      "name": "Sarah Sherali",
+      "city": "The Myriad",
+      "text": "The service provided by dormer's is amazing, they deliver food on time and the food tastes really good which meets ur preferences. Thank you so much, hope the feedback can be helpful someday."
+    },
+    {
+      "id": 11,
+      "from": "bot",
+      "name": "Jannat Kona",
+      "city": "Yugo",
+      "text": "Had a great dinner meal from Dormer's. It was a chicken and egg fried rice. The combination was good. The packaging was good. A bit of more salt and some veggies would have been perfect! A platter including this will be lovely."
+    },
+    {
+      "id": 12,
+      "from": "user",
+      "name": "Bello Hashim",
+      "city": "The Myriad",
+      "text": "Amazing, filling, delicious, good food Their service is wonderful I highly recommend!"
+    },
+    {
+      "id": 13,
+      "from": "bot",
+      "name": "Angela Roy",
+      "city": "Yugo",
+      "text": "some of the best homemade food!! both delicious and healthy"
+    },
+    {
+      "id": 14,
+      "from": "user",
+      "name": "Labeeq Dawre",
+      "city": "Yugo",
+      "text": "Really delicious. The biryani hits the spot"
+    },
+    {
+      "id": 15,
+      "from": "bot",
+      "name": "Syed Abdul Basit",
+      "city": "Yugo",
+      "text": "So far the best quality of food i have tasted. I loved the food. it was amazing & everything simply balanced."
+    },
+    {
+      "id": 16,
+      "from": "user",
+      "name": "Hachem",
+      "city": "KSK homes",
+      "text": "For me the rice was cooked well, and I liked the idea of adding raisins. It added a wonderful flavor."
+    },
+    {
+      "id": 17,
+      "from": "bot",
+      "name": "Suhani Khera",
+      "city": "Vogo Grand",
+      "text": "Love your food, I have Withdrawals."
+    },
+    {
+      "id": 18,
+      "from": "user",
+      "name": "Mirza Alamdar Hussein",
+      "city": "Vogo Grand",
+      "text": "Boss loved the food. Quantity + Quality. Matched the level of expectations."
+    },
+    {
+      "id": 19,
+      "from": "bot",
+      "name": "Rabiya Bi",
+      "city": "KSK Homes",
+      "text": "Alhamdulillah the food has been good. I feel like personally for me, it's a little less spice but it's good. A lot better than the other tiffin services in terms of quality. Thank you."
+    },
+    {
+      "id": 20,
+      "from": "user",
+      "name": "Hafsa Ayesha",
+      "city": "Yugo",
+      "text": "Hey. The food is soooo good. The taste and everything is 10/10/ reminded me of my mom's food back home in Pakistan 🫶. Loved every bit of it."
+    },
+    {
+      "id": 21,
+      "from": "bot",
+      "name": "Adriel Almeida",
+      "city": "Yugo",
+      "text": "...and Ive tried a few meal plans before but nothing compares to this..."
+    }
+  ]
 
 const TestimonialsDesktop = () => {
   const { theme } = useTheme();
@@ -90,7 +175,7 @@ const TestimonialsDesktop = () => {
   }, [groupedMessages.length]);
 
   const positions = ["top-0 left-0", "top-10 right-0", "bottom-0 left-1/3"];
- const handleDotClick = (index) => setCurrentGroup(index);
+  const handleDotClick = (index) => setCurrentGroup(index);
   return (
     <div>
       <div className="w-full py-12 flex justify-center bg-[#031624]">
@@ -111,18 +196,19 @@ const TestimonialsDesktop = () => {
                 >
                   <div
                     key={msg.id}
-                    className={`absolute ${position} ${
-                      isLight ? "bg-[#EEE9DA]" : "bg-[#F4F1EC]"
-                    } rounded-lg p-4 shadow-lg max-w-[280px]`}
+                    className={`absolute ${position} ${isLight ? "bg-[#EEE9DA]" : "bg-[#F4F1EC]"
+                      } rounded-lg p-4 shadow-lg max-w-[280px]`}
                   >
                     <div className="flex items-center gap-2 mb-2">
                       <div className="bg-[#1e3b50] text-white rounded-full w-6 h-6 flex items-center justify-center text-sm font-semibold">
                         {msg.name[0]}
                       </div>
-                      <p className="text-sm font-semibold text-black">
-                        {msg.name}
-                      </p>
-                      <div className="flex ml-auto text-yellow-500 gap-[3px]">
+                      <div>
+                        <p className="text-sm font-semibold text-black">
+                          {msg.name}
+                        </p>
+                        <p className="text-xs font-normal">{msg.city}</p>                      </div>
+                      {/* <div className="flex ml-auto text-yellow-500 gap-[3px]">
                         {[...Array(msg.rating)].map((_, i) => (
                           // <FaStar key={i} className="text-sm" />
                           <Image
@@ -139,7 +225,7 @@ const TestimonialsDesktop = () => {
                             priority
                           />
                         ))}
-                      </div>
+                      </div> */}
                     </div>
                     <p
                       className="text-sm text-gray-800 font-medium"
@@ -152,11 +238,9 @@ const TestimonialsDesktop = () => {
                       {msg.text}
                     </p>
                     <div
-                      className={`absolute -bottom-2 ${
-                        msg.from === "user" ? "right-4" : "left-4"
-                      } w-4 h-4 transform rotate-45 ${
-                        theme === "light" ? "bg-[#EEE9DA]" : "bg-[#EEE9DA]"
-                      }`}
+                      className={`absolute -bottom-2 ${msg.from === "user" ? "right-4" : "left-4"
+                        } w-4 h-4 transform rotate-45 ${theme === "light" ? "bg-[#EEE9DA]" : "bg-[#EEE9DA]"
+                        }`}
                     />
                   </div>
                 </motion.div>
@@ -170,11 +254,10 @@ const TestimonialsDesktop = () => {
           <button
             key={index}
             onClick={() => handleDotClick(index)}
-            className={`w-2.5 h-2.5 rounded-full transition-all duration-300 ${
-              index === currentGroup
-                ? "bg-white scale-110 w-[60px] h-[8px]"
-                : "bg-white/30"
-            }`}
+            className={`w-2.5 h-2.5 rounded-full transition-all duration-300 ${index === currentGroup
+              ? "bg-white scale-110 w-[60px] h-[8px]"
+              : "bg-white/30"
+              }`}
           />
         ))}
       </div>
