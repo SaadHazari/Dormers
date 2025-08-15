2c4359a5a42d0305ec2b0d4fc3568ee273fbdeb0
Adil nawaz <adilnawaz256@gmail.com>
2025-08-15 23:00:51 +0530

    add con closed

--- src/app/components/ChatButton.tsx
+++ src/app/components/ChatButton.tsx
@@ -1,7 +1,24 @@
 'use client';
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
 export default function ChatButton() {
+    const [isChatOpen, setIsChatOpen] = useState(false);
+
+  useEffect(() => {
+    const handleOpen = () => setIsChatOpen(true);
+    const handleClose = () => setIsChatOpen(false);
+
+    window.addEventListener('open-chat', handleOpen);
+    window.addEventListener('close-chat', handleClose);
+
+    return () => {
+      window.removeEventListener('open-chat', handleOpen);
+      window.removeEventListener('close-chat', handleClose);
+    };
+  }, []);
+
+  if (isChatOpen) return null;
   return (
     <button 
       onClick={() => window.dispatchEvent(new CustomEvent('open-chat'))}
