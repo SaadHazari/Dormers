5d0d37432de6911c56c3c1fe3edc7d9150fd7619
saad Hazari <149239774+SaadHazari@users.noreply.github.com>
2026-01-25 13:39:49 +0400

    Integrate Google Ads script in layout.tsx
    Added Google Ads tracking script to layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
@@ -28,6 +29,20 @@ export default function RootLayout({
         />
       </head>
       <body className={montserrat.className}>
+        {/* --- START OF GOOGLE ADS CODE --- */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=AW-17901506705"
+        strategy="afterInteractive"
+      />
+      <Script id="google-ads-tag" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-17901506705');
+        `}
+      </Script>
+      {/* --- END OF GOOGLE ADS CODE --- */}
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
