df1f81b5aa9b97f30c9c4acd7d0858e1bc44a0e0
saad Hazari <149239774+SaadHazari@users.noreply.github.com>
2026-04-11 17:44:18 +0400

    Remove key prop from MobileMenuCard
    Removed key prop from draggable component.

--- src/app/components/MobileMenuCard.tsx
+++ src/app/components/MobileMenuCard.tsx
@@ -98,7 +98,6 @@ export default function MobileMenuCard({
             animate={{ opacity: 1, x: 0 }}
             exit={{ opacity: 0, x: -50 }}
             transition={{ type: "spring", stiffness: 300, damping: 25 }}
-          key={currentDish.id}
           drag="x"
           dragConstraints={{ left: 0, right: 0 }}
           dragElastic={0.5}
